5316290dc6d446f43f02644146a5e7b3354201f5
Tr00d <guillaume.faas@live.fr>
2022-10-28 09:12:07 +0200

    Property refactoring

--- PetProject.Application.Tests.Csharp/ProgramProperties.cs
+++ PetProject.Application.Tests.Csharp/ProgramProperties.cs
@@ -9,34 +9,38 @@ public class ProgramProperties
     [Property]
     public Property DownIncreasesAim() =>
         Prop.ForAll(
+            Arb.From<SubmarineState>(),
             Arb.From<int>(),
-            Arb.From<int>(),
-            (initial, value) => new Program.Submarine(initial, default, default).ExecuteCommand($"down {value}").Aim
-                .Should().Be(value + initial));
+            (state, value) => FromState(state)
+                .ExecuteCommand($"down {value}")
+                .Should()
+                .Be(FromState(state with {Aim = state.Aim + value})));
 
     [Property]
     public Property UpDecreasesAim() =>
         Prop.ForAll(
+            Arb.From<SubmarineState>(),
             Arb.From<int>(),
-            Arb.From<int>(),
-            (initial, value) => new Program.Submarine(initial, default, default).ExecuteCommand($"up {value}").Aim
-                .Should().Be(initial - value));
+            (state, value) => FromState(state)
+                .ExecuteCommand($"up {value}")
+                .Should()
+                .Be(FromState(state with {Aim = state.Aim - value})));
 
     [Property]
-    public Property ForwardIncreasesPosition() =>
+    public Property ForwardIncreasesPositionAndDepth() =>
         Prop.ForAll(
+            Arb.From<SubmarineState>(),
             Arb.From<int>(),
-            Arb.From<int>(),
-            (initial, value) => new Program.Submarine(default, initial, default).ExecuteCommand($"forward {value}")
-                .Position
-                .Should().Be(initial + value));
+            (state, value) =>
+                FromState(state)
+                    .ExecuteCommand($"forward {value}")
+                    .Should()
+                    .Be(FromState(state with
+                    {
+                        Position = state.Position + value, Depth = state.Depth + value * state.Aim,
+                    })));
 
-    [Property]
-    public Property ForwardIncreasesDepth() =>
-        Prop.ForAll(
-            Arb.From<int>(),
-            Arb.From<int>(),
-            (initialAim, forward) =>
-                new Program.Submarine(initialAim, default, default).ExecuteCommand($"forward {forward}").Depth
-                    .Should().Be(forward * initialAim));
+    private static Program.Submarine FromState(SubmarineState state) => new(state.Aim, state.Position, state.Depth);
+
+    private record SubmarineState(int Aim, int Position, int Depth);
 }
